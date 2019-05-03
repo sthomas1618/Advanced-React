edba4c17004c6c200c0893e1880e2bea00621f41
Stephen Thomas <sthomas1618@gmail.com>
2019-05-03 16:23:23 -0700

    part 7: global styles and nav links

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,16 +1,26 @@
 import React from 'react'
-
 import Link from 'next/link'
 
+import NavStyles from './styles/NavStyles'
+
 const Nav = () => (
-  <div>
+  <NavStyles>
+    <Link href="/items">
+      <a>Item</a>
+    </Link>
     <Link href="/sell">
-      <a>Sell!</a>
+      <a>Sell</a>
     </Link>
-    <Link href="/">
-      <a>Home!</a>
+    <Link href="/signup">
+      <a>Signup</a>
     </Link>
-  </div>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Me</a>
+    </Link>
+  </NavStyles>
 )
 
 export default Nav
